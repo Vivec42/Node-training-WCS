f1112daebbbb677c405aee1cbe9041e4b1bbc08d
Quentin Silmain <quentin1813@yahoo.fr>
2023-03-16 10:30:46 +0100

    Nodded

---
+++ index.js
@@ -0,0 +1,6 @@
+import chalk from "chalk";
+
+console.log(chalk.magenta("Camille"));
+console.log(chalk.yellow("Ilias"));
+console.log(chalk.bold.cyan.bgBlueBright("Luc Marcheciel"));
+console.log(chalk.green("Jessica"));
